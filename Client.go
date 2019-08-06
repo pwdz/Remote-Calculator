398abb20464c62552720eda05db7f653088d4245
Ebi <9731002@ceit.local>
2019-08-06 23:49:47 +0430

    client is ready

--- Client.go
+++ Client.go
@@ -8,10 +8,11 @@ import (
 )
 
 func main() {
-	conn, _ := net.Dial("tcp", HOST+":"+PORT)
+	conn, _ := net.Dial("tcp", "localhost"+":"+"1540")
 	for {
 		//reader := bufio.NewReader(os.Stdin)
 		var input string
+		fmt.Println("Enter input")
 		_, _ = fmt.Scanln(&input)
 		//text, _ := reader.ReadString('\n')
 		// send to socket
@@ -20,7 +21,8 @@ func main() {
 		//message, _ := bufio.NewReader(conn).ReadString('\n')
 
 		//fmt.Print("Message from server: " + message)
-		_, _ = conn.Write([]byte(string(input)))
+		fmt.Println(input)
+		_, _ = conn.Write([]byte(string(input+"\n")))
 		res,_ :=bufio.NewReader(conn).ReadString('\n')
 		fmt.Println("result is: "+res)
 		if strings.Compare(input, "finish") == 0 {
--- Server.go
+++ Server.go
@@ -1,22 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
+	""
 )
 const(
-	ConnectionPort = "1540"
-	ConnectionHost = "localhost"
+	PORT = "1540"
+	HOST = "localhost"
 )
 func main()  {
-	socket,err := net.Listen("tcp",ConnectionHost+":"+ConnectionPort)
+	socket,err := net.Listen("tcp",HOST+":"+PORT)
 	if err!=nil{
 		fmt.Println("error listening")
 		os.Exit(-1)
 	}
 	defer socket.Close()
-	fmt.Println("Listening on " + ConnectionHost + ":" + ConnectionPort)
+	fmt.Println("Listening on " + HOST+ ":" + PORT)
 	fmt.Println(socket)
 	for{
 		conn,err:=socket.Accept()
@@ -24,9 +27,25 @@ func main()  {
 			fmt.Println("Error accepting: ", err.Error())
 			os.Exit(1)
 		}
+		fmt.Println("Connected!")
 		go handleRequest(conn)
 	}
 }
 func handleRequest(conn net.Conn)  {
-	
+	//buf := make([]byte, 1024)
+	// Read the incoming connection into the buffer.
+	//req, err := conn.Read(buf)
+	for {
+		netData, err := bufio.NewReader(conn).ReadString('\n')
+		fmt.Println("you sent this:"+netData)
+		if err != nil {
+			fmt.Println("Error reading:", err.Error())
+		}
+		if strings.Compare(string(netData), "finish") == 0 {
+			break
+		}
+		fmt.Println("calculating...")
+		_, _ = conn.Write([]byte(string(main.calculate(netData))))
+	}
+	_ = conn.Close()
 }
